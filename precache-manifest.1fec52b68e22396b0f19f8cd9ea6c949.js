c6ec0afaff42591b4443252e03d7386ff3b8c8a0
ARochniak <15bblinn@gmail.com>
2019-09-21 19:37:44 +0300

    Updates

---
+++ precache-manifest.1fec52b68e22396b0f19f8cd9ea6c949.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ae7cfbb64d822977e831473d1b02eb8",
+    "url": "/react_redux_Calculator/index.html"
+  },
+  {
+    "revision": "71f2f92776b8e97d95ac",
+    "url": "/react_redux_Calculator/static/css/main.87bb4fd5.chunk.css"
+  },
+  {
+    "revision": "2f47b88307249fd779ca",
+    "url": "/react_redux_Calculator/static/js/2.fb8bc725.chunk.js"
+  },
+  {
+    "revision": "71f2f92776b8e97d95ac",
+    "url": "/react_redux_Calculator/static/js/main.c108540f.chunk.js"
+  },
+  {
+    "revision": "e16bf10ed102e4f92858",
+    "url": "/react_redux_Calculator/static/js/runtime~main.4f6efc31.js"
+  },
+  {
+    "revision": "ce418eca8129ebc90207109afa967c8d",
+    "url": "/react_redux_Calculator/static/media/click.ce418eca.mp3"
+  }
+]);
